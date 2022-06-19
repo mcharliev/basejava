0700342316e5570893a1611d18a5d0e864af43b6
Merdan Charliev <mcharliev@gmail.com>
2022-06-19 15:01:09 +0300

    FirstH_W

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,52 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+
+    }
+    void update(Resume r) {
+
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()) {
+                storage[i] = storage[size - 1];
+                storage[i] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
